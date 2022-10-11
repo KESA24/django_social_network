f0c2f7a52f51ef96f4fbbca122f402ab54c11894
Patricia Rhoda Nekesa <patricia@emata.io>
2022-10-11 17:55:03 +0300

    create-room pause

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -67,7 +67,8 @@ def home(request):
         )
     topics = Topic.objects.all()
     room_count = rooms.count()
-    room_messages = Message.objects.all()
+    # Only activity/messages from a specific room
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
     
     context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages': room_messages}
@@ -92,21 +93,47 @@ def room(request, pk):
     context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
+
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    # get all the user's rooms
+    rooms = user.room_set.all()
+    # get all users' room messages and topics
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user, 'rooms': rooms,
+               'room_messages': room_messages, 'topics': topics}
+    return render(request, 'base/profile.html', context)
+
+
 @login_required(login_url='/login')
 def createRoom (request):
+    topics = Topic.objects.all()
     form = RoomForm()
     if request.method == "POST":
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    context = {'form': form}
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+
+        Room.objects.create(
+            host=request.user,
+            topic = topic,
+            name=request.POST.get('name'),
+            description = request.POST.get('description')
+        )
+        # form = RoomForm(request.POST)
+        # if form.is_valid():
+        #     room = form.save(commit=False) 
+        #     room.host = request.user
+        #     room.save()
+        #     return redirect('home')
+    context = {'form': form, 'topics': topics}
     return render(request, 'base/room_form.html', context)
 
 @login_required(login_url='/login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    topics = Topic.objects.all()
 
     if request.user != room.host:
         return HttpResponse('Your are not allowed here!!')
@@ -116,7 +143,7 @@ def updateRoom(request, pk):
         if form.is_valid():
             form.save()
             return redirect('home')
-    context = {'form': form}
+    context = {'form': form, 'topics': topics, 'room': room}
     return render(request,'base/room_form.html', context)
 
 @login_required(login_url='/login')
